9f9864522a387305b395d3dd4939feca75a28ac2
smheidrich <smheidrich@weltenfunktion.de>
2021-03-29 04:55:34 +0200

    Fix pathlib issue

--- sphinxcontrib/repl_selectability/__init__.py
+++ sphinxcontrib/repl_selectability/__init__.py
@@ -4,6 +4,7 @@ from sphinx.util.fileutil import copy_asset
 
 # how to add JS+CSS from:
 # https://github.com/sphinx-doc/sphinx/issues/1379#issuecomment-809006086
+# TODO: open sphinx ticket about documenting copy_asset somewhere
 
 # resource path from https://stackoverflow.com/a/58941536/2748899 , although it
 # seems that there is some controversy as to what is the "future-proof" method
@@ -17,9 +18,11 @@ def copy_asset_files(app, exc):
   if exc is None: # build succeeded
     for path in asset_files:
       with importlib.resources.path(__name__, "repl-selectability.js") as p:
-        copy_asset(p, os.path.join(app.outdir, '_static'))
+        # XXX: open sphinx ticket regarding copy_asset being able to take
+        # pathlib.Path so I don't have to convert it to str here
+        copy_asset(str(p), os.path.join(app.outdir, '_static'))
       with importlib.resources.path(__name__, "repl-selectability.css") as p:
-        copy_asset(p, os.path.join(app.outdir, '_static'))
+        copy_asset(str(p), os.path.join(app.outdir, '_static'))
 
 def setup(app):
   app.connect('builder-inited', inject_js_css_paths)
