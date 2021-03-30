926868fc3d9b3cb761a63ef20f314a5ba3b69f82
smheidrich <smheidrich@weltenfunktion.de>
2021-03-30 23:16:15 +0200

    Make output selectab. linewise, prompt v. noprompt

--- sphinxcontrib/repl_selectability/repl-selectability.js
+++ sphinxcontrib/repl_selectability/repl-selectability.js
@@ -7,12 +7,47 @@ function hasPrompts(highlightElem) {
   return highlightElem.getElementsByClassName('gp').length > 0;
 }
 
+/* wrapping text node stuff based on:
+ * https://coryrylan.com/blog/wrapping-dom-text-nodes-with-javascript
+ */
+
+function getAllTextNodes(element) {
+  return Array.from(element.childNodes).filter(
+    node => (node.nodeType == Node.TEXT_NODE && node.textContent.length >= 1));
+}
+
+function wrapFreeTextInSpans(element) {
+  /* this is too complicated to understand so if it breaks just write it again
+   * from scratch; what it does is it splits up text nodes into smaller text
+   * nodes (using JS native node.splitText) such that newlines always appear at
+   * the end of a node; this is important for how things will be divided into
+   * "prompt lines" and "non-prompt lines" in the output selectability code
+   * below */
+  getAllTextNodes(element).forEach(node => {
+    while (true) {
+      var match = node.textContent.match(/\r\n|\r|\n/);
+      if (match != null) {
+        var nodeAfter = node.splitText(match.index+match.length);
+      }
+      const span = document.createElement('span');
+      node.after(span);
+      span.appendChild(node);
+      if (match == null) {
+        break;
+      } else {
+        node = nodeAfter;
+      }
+    }
+  });
+}
+
 window.onload = function() {
   var highlightElems = document.getElementsByClassName('highlight');
   for (let highlightElem of highlightElems) {
     if (!hasPrompts(highlightElem)) {
       continue;
     }
+    wrapFreeTextInSpans(highlightElem.firstChild);
     var codeSettingsNode = document.createElement("div");
     codeSettingsNode.classList.add('codesettings');
     codeSettingsNode.innerHTML = '<form></form>';
@@ -27,28 +62,24 @@ window.onload = function() {
 
     selectOutputsNode.firstChild.addEventListener("change", (ev) => {
       var c = ev.target.checked;
-      /* Considering what we want is "all elements between certain other
-       * elements", there might be a way to simplify this usign CSS:
-       * https://stackoverflow.com/a/42575222/2748899
-       * But I'm not a webdev so will have to research a bit more.
-       */
-      var inOutputBlock = false;
+      var inPromptLine = false;
       for (subelem of highlightElem.firstChild.children) {
-        if (["go", "gr", "ge"].some((x) => subelem.classList.contains(x))) {
-          inOutputBlock = true;
-          console.log(subelem);
-        } else if (subelem.classList.contains("gp")) {
-          inOutputBlock = false;
+        if (subelem.classList.contains("gp")
+        && subelem.innerHTML.search("\n") == -1) {
+          inPromptLine = true;
         }
-        if (inOutputBlock) {
+        if (!inPromptLine) {
           subelem.style.userSelect = c ? 'auto' : 'none';
+        }
+        if (subelem.innerHTML.search("\n") != -1) {
+          inPromptLine = false;
         }
       }
     });
 
     selectPromptsNode.firstChild.addEventListener("change", (ev) => {
+      var c = ev.target.checked;
       for (subelem of highlightElem.getElementsByClassName('gp')) {
-        var c = ev.target.checked;
         subelem.style.userSelect = c ? 'auto' : 'none';
       }
     });
